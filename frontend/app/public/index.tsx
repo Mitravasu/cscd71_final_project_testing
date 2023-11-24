05ba764df2f7b6295cde7c63ae24c8628dbabf92
kouroshjaberi11 <kourosh.jaberi@mail.utoronto.ca>
2023-11-23 22:27:50 -0500

    Added public folder for chrome extension - but can't find default_popup page

---
+++ frontend/app/public/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head"
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <h1>
+          Hello World
+        </h1>
+      </main>
+    </div>
+  );
+}
