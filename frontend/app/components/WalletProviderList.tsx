da5b8bd3fe6f7d6721674c36c03decb8d1bf0f10
Mitravasu Prakash <43656604+Mitravasu@users.noreply.github.com>
2023-11-25 17:40:21 -0500

    Mock Extension

--- frontend/app/components/WalletProviderList.tsx
+++ frontend/app/components/WalletProviderList.tsx
@@ -3,23 +3,6 @@ import { useState, useEffect } from "react";
 import { Eip1193Provider, ethers } from "ethers";
 import Image from "next/image";
 
-interface EIP6963ProviderInfo {
-  uuid: string;
-  name: string;
-  icon: string;
-  rdns: string;
-}
-
-interface EIP6963ProviderDetail {
-  info: EIP6963ProviderInfo;
-  provider: Eip1193Provider;
-}
-
-interface EIP6963AnnounceProviderEvent extends CustomEvent {
-  type: "eip6963:announceProvider";
-  detail: EIP6963ProviderDetail;
-}
-
 declare global {
   interface WindowEventMap {
     "eip6963:announceProvider": EIP6963AnnounceProviderEvent;
@@ -48,12 +31,19 @@ export default function WalletProviderList() {
     window.dispatchEvent(new Event("eip6963:requestProvider"));
   }
 
+  function printProviderInfo() {
+    console.log(providers);
+  }
+
   function createProviderCard() {
     return providers?.map((provider, index) => {
       return (
-        <div key={index} className="flex flex-col font-mono text-sm bg-slate-800 p-4 rounded-lg text-center mt-10">
-          <Image src={provider.info.icon} width={96} height={96} alt=""/>
-          <h1 className="text-white p-2" >{provider.info.name}</h1>
+        <div
+          key={index}
+          className="flex flex-col font-mono text-sm bg-slate-800 p-4 rounded-lg text-center mt-10"
+        >
+          <Image src={provider.info.icon} width={96} height={96} alt="" />
+          <h1 className="text-white p-2">{provider.info.name}</h1>
         </div>
       );
     });
@@ -63,6 +53,7 @@ export default function WalletProviderList() {
     <main>
       <div className="flex flex-col items-center font-mono text-sm lg:flex">
         <h1>Available Wallets</h1>
+        <button onClick={printProviderInfo}>Print Provider Info</button>
         {createProviderCard()}
       </div>
     </main>
---
+++ frontend/types.d.ts
@@ -0,0 +1,21 @@
+
+interface EIP6963ProviderInfo {
+  uuid: string;
+  name: string;
+  icon: string;
+  rdns: string;
+}
+
+interface EIP6963ProviderDetail {
+  info: EIP6963ProviderInfo;
+  provider: Eip1193Provider;
+}
+
+interface EIP6963AnnounceProviderEvent extends CustomEvent {
+  type: "eip6963:announceProvider";
+  detail: EIP6963ProviderDetail;
+}
+
+interface EIP6963RequestProviderEvent extends Event {
+  type: "eip6963:requestProvider";
+}
